854c688a8f0a1a580c513af0b8a4ea78e2810e5b
Praveen <tidancadlab@gmail.com>
2024-03-20 19:59:41 +0530

    Added cart functionality

--- src/component/Menu/index.jsx
+++ src/component/Menu/index.jsx
@@ -1,18 +1,18 @@
 const Menu = ({items, onCategory, category}) => (
-  <div className="border-b">
+  <div className='border-b'>
     <div
-      role="radiogroup"
+      role='radiogroup'
       aria-required
-      aria-labelledby="abc"
-      className="flex gap-2 overflow-y-auto scrollbar no-scrollbar"
+      aria-labelledby='abc'
+      className='flex gap-2 overflow-y-auto scrollbar no-scrollbar'
     >
       {items.map((v, i) => (
         <button
           key={v.menu_category_id}
-          type="button"
+          type='button'
           onClick={() => onCategory(i)}
           data-active={i === category}
-          className="px-4 py-2 data-[active=true]:text-secondary hover:text-secondary/70 border-b-2 border-transparent data-[active=true]:border-secondary text-nowrap ease-in-out duration-200"
+          className='px-4 py-2 data-[active=true]:text-secondary hover:text-secondary/70 border-b-2 border-transparent data-[active=true]:border-secondary text-nowrap ease-in-out duration-200'
         >
           {v.menu_category}
         </button>
